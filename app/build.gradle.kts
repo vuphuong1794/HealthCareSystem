86dc42c55c5baf5f75874cf8364227f1c4e2a7c1
vuphuong1794 <phuongvu1794@gmail.com>
2025-03-11 15:56:59 +0700

    add api to login page and decode jwt accesstoken and print username to mainpage

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -52,4 +52,5 @@ dependencies {
     androidTestImplementation(libs.androidx.espresso.core)
     implementation ("com.squareup.retrofit2:retrofit:2.9.0")
     implementation ("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation ("com.auth0.android:jwtdecode:2.0.2")
 }
--- app/src/main/java/com/example/healthcaresystem/model/SignUpResponse.kt
+++ app/src/main/java/com/example/healthcaresystem/model/SignUpResponse.kt
@@ -4,3 +4,4 @@ package com.example.healthcaresystem.model
 data class SignUpResponse(
     val message: String
 )
+
